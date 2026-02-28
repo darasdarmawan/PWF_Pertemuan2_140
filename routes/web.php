11b6b1a4ee604bd126225e498b9e801b4a8a11fa
darasdarmawan <darasyauqidarmawan@gmail.com>
2026-03-01 01:11:03 +0700

    menambahkan route untuk halaman about

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,7 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    Route::get('/about', [AboutController::class, 'index'])->name('about'); #Tambahkan route untuk halaman about
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
